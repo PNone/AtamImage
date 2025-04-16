992e74cca67e61370d7b0785e44e53263b4469a3
PNone <urikita8@gmail.com>
2025-04-16 16:20:00 +0300

    Add Build wsl for OpSys Image

---
+++ OparatingSystems/build_wsl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+container_id=$(docker run -t -d --name OpSysUbuntu opsys-build:${OP_SYS_TAG} /bin/bash)
+docker export --output=OpSysUbuntu.tar $container_id
+wsl.exe --import OpSysUbuntu . OpSysUbuntu.tar
+
+# Run machine using: 
+# wsl -d OpSysUbuntu
